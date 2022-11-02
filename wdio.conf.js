51f933cdea72566eac2bcef0a9b72c3b5550b4e6
saucechaffe <shane.chaffe@saucelabs.com>
2022-11-02 22:44:49 +0000

    100 jobs

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,9 +23,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: [
-        './test/**.js'
-    ],
+    specs: Array(100).fill('./test/*.js'),
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
