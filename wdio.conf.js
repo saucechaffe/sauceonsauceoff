5d671aba2606725c7b2bf11aac4db512b3188599
saucechaffe <shane.chaffe@saucelabs.com>
2022-11-03 09:37:54 +0000

    edit conf for instances

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    specs: Array(100).fill('./test/*.js'),
+    specs: Array(50).fill('./test/*.js'),
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
@@ -44,7 +44,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 100,
+    maxInstances: 20,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
