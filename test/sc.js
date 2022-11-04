cdade4bf55eb32c8d3f34028bced755175caf84f
saucechaffe <shane.chaffe@saucelabs.com>
2022-11-04 16:09:00 +0000

    change site cos captcha

--- test/sc.js
+++ test/sc.js
@@ -5,15 +5,15 @@ console.log("private ip address", a);
 
 describe('Confirms whether you are using Sauce Connect or not', () => {
     it('should open a webpage', async() => {
-        await browser.url('https://www.whatismyip.com/');
+        await browser.url('https://ip-checker.shanechaffe.repl.co/');
         await browser.pause(10000);
-        await expect(browser).toHaveTitle('What Is My IP? Shows Your Public IP Address - IPv4 - IPv6');
+        await expect(browser).toHaveTitle('My IP');
     });
     it('copies IP to console', async() => {
         // const disagree = await $('//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]');
         // await disagree.click();
 
-        const myIP = await $('//*[@id="ipv4"]/a');
+        const myIP = await $('//*[@id="ipHere"]');
         console.log(await myIP.getText());
         // in real test, uncomment this for mobile so it can load the full recording in sauce ui
         await browser.pause(30000);
