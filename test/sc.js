cc528086776e2946fb9c89b52695ee11f0ee19a7
saucechaffe <shane.chaffe@saucelabs.com>
2022-11-05 21:01:09 +0000

    change site due to flake

--- test/sc.js
+++ test/sc.js
@@ -5,15 +5,15 @@ console.log("private ip address", a);
 
 describe('Confirms whether you are using Sauce Connect or not', () => {
     it('should open a webpage', async() => {
-        await browser.url('https://ip-checker.shanechaffe.repl.co/');
+        await browser.url('https://nordvpn.com/what-is-my-ip/');
         await browser.pause(10000);
-        await expect(browser).toHaveTitle('My IP');
+        await expect(browser).toHaveTitle('What is my IP address location? Find out here | NordVPN');
     });
     it('copies IP to console', async() => {
         // const disagree = await $('//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]');
         // await disagree.click();
 
-        const myIP = await $('//*[@id="ipHere"]');
+        const myIP = await $('/html/body/div[3]/div[2]/div/div/div/div/div/div/div[1]/h1[2]');
         console.log(await myIP.getText());
         // in real test, uncomment this for mobile so it can load the full recording in sauce ui
         await browser.pause(30000);
